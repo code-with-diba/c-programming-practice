f0fe65bc1a8e253b47ac1e0c12ad79e994ca8a09
code-with-diba <dibamutsuddy321@gmail.com>
2026-08-18 09:01:39 +0600

    Create day03-absolute-value.c

---
+++ day03-absolute-value.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+#include<stdlib.h>
+int main()
+{
+    int number,absolute_value;
+    scanf("%d",&number);
+    absolute_value = abs(number);
+    printf("Absolute value = %d",absolute_value);
+}
