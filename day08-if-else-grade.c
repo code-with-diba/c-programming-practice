e39b31b8d91a6950023892f17b30369ba5c8c6d8
code-with-diba <dibamutsuddy321@gmail.com>
2026-08-23 17:36:28 +0600

    Create day08-if-else-grade.c

---
+++ day08-if-else-grade.c
@@ -0,0 +1,29 @@
+#include<stdio.h>
+int main()
+{
+    float m;
+    scanf("%f",&m);
+
+    if(m >= 90)
+        printf("A");
+
+    else if(m >= 80)
+        printf("B");
+
+    else if(m >= 70)
+        printf("C");
+
+    else if(m >= 60)
+        printf("D");
+
+    else if(m >= 60)
+        printf("D");
+
+    else if(m >= 40)
+        printf("E");
+
+    else
+        printf("F");
+}
+
+    }
