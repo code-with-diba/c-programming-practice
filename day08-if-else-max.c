1ab0450ede477259e16b73ca9c315d81964bb5eb
code-with-diba <dibamutsuddy321@gmail.com>
2026-08-23 13:17:25 +0600

    Create day08-if-else-max.c

---
+++ day08-if-else-max.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main()
+{
+    int a,b;
+    scanf("%d %d",&a,&b);
+
+    if(a>b)
+        printf("maximum = %d",a);
+    else if(b>a)
+        printf("maximum = %d",b);
+    else
+        printf("Numbers are equal");
+}
