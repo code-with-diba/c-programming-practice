a5b71150c02aa7aa00eba9d3255e8bde2ff06159
code-with-diba <dibamutsuddy321@gmail.com>
2026-08-23 13:23:57 +0600

    Create day08-if-else-syntax-multiple-statement.c

---
+++ day08-if-else-syntax-multiple-statement.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main()
+{
+    int time;
+    scanf("%d",&time);
+
+    if(time == 10)
+    {
+        printf("Good morning\n");
+        printf("Diba Mutsuddy\n");
+    }
+    else
+    {
+        printf("Sorry Its not morning\n");
+        printf("Diba Mutsuddy\n");
+    }
+}
