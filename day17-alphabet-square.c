37d7924e38787531f418d97c16edf49c3475fb10
code-with-diba <dibamutsuddy321@gmail.com>
2026-09-03 22:07:58 +0600

    Create day17-alphabet-square.c

---
+++ day17-alphabet-square.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main()
+{
+    int n,a,d,i,j;
+    scanf("%d",&n);
+
+    for(i=1;i<=n;i++){
+        a=1;
+        for(j=1;j<=n;j++){
+           d=a+64;
+           char ch = (char)d;
+           printf("%c ",ch);
+           a++;
+        }
+        printf("\n");
+          }
+}
