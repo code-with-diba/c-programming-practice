1e26c288e8d0b1c312cb7e094ba4aaf499ea0730
code-with-diba <dibamutsuddy321@gmail.com>
2026-09-03 21:51:17 +0600

    Create day17-odd-number-triangle.c

---
+++ day17-odd-number-triangle.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main()
+{
+    int n,a,i,j;
+    scanf("%d",&n);
+
+    for(i=1;i<=n;i++){
+        a=1;
+        for(j=1;j<=i;j++){
+            printf("%d ",a);
+            a=a+2;
+        }
+        printf("\n");
+          }
+}
