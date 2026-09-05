0ce2259eb57c83f622b01dcaf0232194dc433708
code-with-diba <dibamutsuddy321@gmail.com>
2026-09-06 00:16:38 +0600

    Create day19-star-pyramid.c

---
+++ day19-star-pyramid.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+int main()
+{
+    int n,nst=1,i,j,k;
+    scanf("%d",&n);
+
+    for(i=1;i<=n;i++){
+        for(j=1;j<=n-i;j++){
+            printf(" ");
+        }
+        for(k=1;k<=nst;k++){
+            printf("*");
+        }
+
+        nst=nst+2;
+        printf("\n");
+    }
+}
